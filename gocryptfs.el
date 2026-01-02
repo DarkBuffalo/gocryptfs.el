f1af34a9e792c11e647c990624cd6e204256a59c
DarkBuffalo <darkbuffalo@gnu.re>
2026-01-02 19:36:40 +0100

    add: security

--- gocryptfs.el
+++ gocryptfs.el
@@ -1,11 +1,11 @@
-;;; gocryptfs.el --- goCryptFs Tool
+;;; gocryptfs.el --- goCryptFs Tool -*- lexical-binding: t; -*-
 ;;
 ;; Filename: gocryptfs.el
 ;; Description:
 ;; Author: Matthias David <darkbuffalo@gnu.re>
 ;; Created: Tue Jun 27 15:29:11 2023 (+0200)
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.4") (epg))
+;; Package-Requires: ((emacs "24.4") (epg) (cl-lib))
 
 ;;; Commentary:
 ;; This allows me to mount a private directory encrypted using gocryptfs.
@@ -14,40 +14,56 @@
 ;;; Code:
 
 (require 'epg)
+(require 'cl-lib)
 
-(defgroup gocrytfs nil
+(defgroup gocryptfs nil
   "Mount a private directory encrypted using gocryptfs."
   :group 'crypto)
 
-(defcustom gocryptfs-private-dir-name "Perso"
+(defcustom gocryptfs-private-dir-name "~/Perso"
   "Gocryptfs private directory name."
   :type 'string
   :group 'gocryptfs)
 
-(defcustom gocryptfs-root-dir "~/.perso.tomb/"
+(defcustom gocryptfs-root-dir "~/.tomb/"
   "Gocryptfs root configuration directory."
   :type 'directory
   :group 'gocryptfs)
 
 (defun gocryptfs-config-file ()
-  (concat gocryptfs-root-dir "gocryptfs.conf"))
+  (expand-file-name "gocryptfs.conf" gocryptfs-root-dir))
 
 (defvar gocryptfs-buffer-name " *emacs-gocryptfs*")
-(defvar gocryptfs-process-name "emacs-gocryptfs")
-(defvar gocryptfs--mount-private-cmd "gocryptfs")
-(defvar gocryptfs--umount-private-cmd "fusermount")
 
+(defvar gocryptfs-process-name "emacs-gocryptfs")
+
+(defcustom gocryptfs--mount-private-cmd "gocryptfs"
+  "Command used to mount gocryptfs")
+
+(defcustom gocryptfs--umount-private-cmd "fusermount3"
+  "Command used to unmount gocryptfs")
 
 (defun gocryptfs-available-p ()
+"Return non-nil if gocryptfs is correctly installed and configured."
   (and (file-directory-p (expand-file-name gocryptfs-private-dir-name "~"))
-       (cl-every #'file-exists-p (list gocryptfs--mount-private-cmd
-                                       gocryptfs--umount-private-cmd
-                                       (gocryptfs-config-file)))))
+       (executable-find gocryptfs--mount-private-cmd)
+       (executable-find gocryptfs--umount-private-cmd)
+       (file-exists-p (gocryptfs-config-file))))
 
 (defun gocryptfs-private-mounted-p ()
-  (let ((mount (shell-command-to-string "mount")))
-    (and (string-match-p (concat ".*" (expand-file-name gocryptfs-private-dir-name "~") ".*gocryptfs.*") mount)
-         t)))
+  "Return non-nil if the gocryptfs private directory is mounted."
+  (let ((target (expand-file-name gocryptfs-private-dir-name "~")))
+    (condition-case nil
+        (and (process-lines "findmnt"
+                             "-n"
+                             "-o" "FSTYPE"
+                             "--target" target)
+             (member "fuse.gocryptfs"
+                     (process-lines "findmnt"
+                                    "-n"
+                                    "-o" "FSTYPE"
+                                    "--target" target)))
+      (error nil))))
 
 
 ;;;###autoload
@@ -61,19 +77,35 @@
 
 ;;;###autoload
 (defun gocryptfs-mount-private ()
-  "Mount gocryptfs' private directory."
+  "Mount gocryptfs' private directory without blocking Emacs."
   (interactive)
 
-  (let ((try-again t))
-    (while (and ;; In the first iteration, we try to silently mount the gocryptfs private directory,
-            ;; this would succeed if the key is available in the keyring.
-            (prog1 (not (zerop (shell-command gocryptfs--mount-private-cmd gocryptfs-buffer-name)))
-              (message "Successfully mounted private directory."))
-            (prog1 try-again (setq try-again nil)))
-      (if (zerop (shell-command (concat "echo " (read-passwd "Password :") " | " gocryptfs--mount-private-cmd " " gocryptfs-root-dir " " gocryptfs-private-dir-name)))
-          (message "Successfully mounted private directory.")
-        (user-error "A problem occured while mounting the private directory, see %s"
-                    gocryptfs-buffer-name)))))
+  (let* ((buffer   (get-buffer-create gocryptfs-buffer-name))
+         (root     (expand-file-name gocryptfs-root-dir))
+         (target   (expand-file-name gocryptfs-private-dir-name "~"))
+         (password (read-passwd "Password: "))
+         (command  (list gocryptfs--mount-private-cmd
+                         root
+                         target))
+         proc)
+
+    (with-current-buffer buffer
+      (erase-buffer))
+
+    (setq proc
+          (make-process
+           :name gocryptfs-process-name
+           :buffer buffer
+           :stderr buffer
+           :command command
+           :noquery t))
+
+    ;; envoyer le mot de passe immédiatement
+    (process-send-string proc (concat password "\n"))
+    (process-send-eof proc)
+
+    (message "Mounting private directory...")))
+
 
 ;;;###autoload
 (defun gocryptfs-umount-private ()
@@ -82,8 +114,6 @@
   (if (zerop (shell-command (concat gocryptfs--umount-private-cmd " -u ~/" gocryptfs-private-dir-name)))
       (message "Unmounted private directory successfully.")
     (user-error "Cannot unmount the private directory, seems to be already unmounted.")))
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; gocryptfs.el ends here
